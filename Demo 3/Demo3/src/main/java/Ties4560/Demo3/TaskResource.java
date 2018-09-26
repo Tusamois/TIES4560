fe4e84d15b3963dcab5e2f0e80deca879d6f88d0
Tuukka Jurvakainen <tuukkaa.jurvakainen@gmail.com>
2018-09-26 22:53:49 +0300

    demo 3 fixed

--- Demo 3/Demo3/src/main/java/Ties4560/Demo3/TaskResource.java
+++ Demo 3/Demo3/src/main/java/Ties4560/Demo3/TaskResource.java
@@ -17,15 +17,13 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
 @Path("/")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class TaskResource {
 	
 	private TaskService taskService = new TaskService();
-	int groupId;
+	int GroupId;
 	
 	@GET
-	public Response getTasks(@PathParam("groupId") int groupId){
+	public Response getTasks(@PathParam("GroupId") int groupId){
 			List<Task> newTask = taskService.getAllTasks(groupId);
 			return Response.status(Status.OK)
 					.header("User:", "admin")
@@ -35,7 +33,7 @@ public class TaskResource {
 	
 	@GET
 	@Path("/{taskId}")
-	public Response getTask(@PathParam("groupId") int groupId, @PathParam("taskId") int taskId){
+	public Response getTask(@PathParam("GroupId") int groupId, @PathParam("taskId") int taskId){
 	Task newTask = taskService.getTask(groupId, taskId);
 	return Response.status(Status.OK)
 	.header("User:", "admin")
@@ -59,10 +57,10 @@ public class TaskResource {
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response sendTask(@PathParam("groupId") int groupId, Task task, @Context UriInfo uriInfo) {
+	public Response sendTask(@PathParam("GroupId") int groupId, Task task, @Context UriInfo uriInfo) {
 		Task newTask = taskService.sendTask(groupId, task);
 
-		String uri = uriInfo.getBaseUriBuilder().path(GroupResource.class).path(GroupResource.class, "getTaskResource").resolveTemplate("groupId", newTask.getGroupId()).path(Long.toString(newTask.getTaskId())).build().toString();
+		String uri = uriInfo.getBaseUriBuilder().path(GroupResource.class).path(GroupResource.class, "getTaskResource").resolveTemplate("GroupId", newTask.getGroupId()).path(Long.toString(newTask.getTaskId())).build().toString();
 		newTask.addLink(uri, "self");
 		
 		uri = uriInfo.getBaseUriBuilder().path(GroupResource.class).path(Long.toString(newTask.getGroupId())).build().toString();
